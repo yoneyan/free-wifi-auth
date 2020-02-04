ffa7e3ead97e3110014ebbfb871e7d3b3188695e
yoneyan <github@yoneyan.net>
2020-02-05 03:37:23 +0900

    create blank user for testing

--- auth.go
+++ auth.go
@@ -2,6 +2,11 @@ package main
 
 func verifyauth(ip, user, pass string) bool {
 	if user == "test" && pass == "test" {
+		recordclient(ip)
+		return true
+	}
+	if user == "" && pass == "" {
+		recordclient(ip)
 		return true
 	}
 	return false
