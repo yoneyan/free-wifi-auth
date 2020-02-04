def0e8a8ae4950bc3d9a4dcc1a601d0eb2fd8495
yoneyan <github@yoneyan.net>
2020-02-05 03:39:24 +0900

    add confirmation of data periodically

--- timer.go
+++ timer.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+)
+
+func cronfunction() {
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+loop:
+	for {
+		select {
+		case <-sc:
+			fmt.Println("interrupt")
+			break loop
+		case <-time.After(1 * time.Second):
+			clientmonitor()
+		}
+	}
+}
+
+func clientmonitor() {
+	length := clientdatalength()
+	var i int
+	for i = 0; i < length; i++ {
+		//fmt.Println(readclient(i))
+		if readclient(i).Active {
+			if readclient(i).EndClock < time.Now().Unix() {
+				fmt.Println(time.Now().Unix())
+				result := rejectclient(readclient(i).IP)
+				fmt.Println(readclient(i).IP + ": " + strconv.FormatBool(result))
+				if !lockstatus() {
+					writeclientstatus(i, false)
+				}
+			}
+		}
+	}
+}
