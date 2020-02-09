5537143b3264e8f5cd7d2ae9c8e1a2b89a6776f1
yoneyan <github@yoneyan.net>
2020-02-10 03:10:35 +0900

    add user management system

--- auth.go
+++ auth.go
@@ -1,13 +1,63 @@
 package main
 
-func verifyauth(ip, user, pass string) bool {
-	if user == "test" && pass == "test" {
-		recordclient(ip)
-		return true
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"os"
+)
+
+func usersetting() {
+	fmt.Println("------------user setting------------")
+	fmt.Println("1: register,2: delete,3: test")
+	stdin := bufio.NewScanner(os.Stdin)
+	for stdin.Scan() {
+		fmt.Println("--------UserSetting Mode-------")
+		text := stdin.Text()
+		if text == "1" {
+			var user, pass string
+			fmt.Println("input user and password")
+			fmt.Scan(&user, &pass)
+			fmt.Println(addDBUser(user, pass))
+		}
+		if text == "2" {
+			var user string
+			fmt.Println("input user")
+			fmt.Scan(&user)
+			fmt.Println(deleteDBUser(user))
+		}
+		if text == "3" {
+			var user, pass string
+			fmt.Println("input user and password")
+			fmt.Scan(&user, &pass)
+			ip := "test"
+			fmt.Println(verifyauth(ip, user, pass))
+		}
+		if text == "end" {
+			break
+		}
+		fmt.Println("This command is invalid")
+
 	}
-	if user == "" && pass == "" {
-		recordclient(ip)
+	fmt.Println("UserSetting MODE END")
+}
+
+func verifyauth(ip, user, pass string) bool {
+	if TestPassDBUser(user, pass) {
 		return true
 	}
 	return false
+}
+
+func hashgenerate(data string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(data), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(hash)
+}
+
+func verifyhashdata(data string, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(data)) == nil
 }
---
+++ db.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+const db_name = "./main.sql"
+
+func connectdb() *sql.DB {
+	db, err := sql.Open("sqlite3", db_name)
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Println("SQL open error")
+		panic(err)
+	}
+
+	//defer db.Close()
+
+	return db
+}
+
+func createdb(database string) bool {
+	db := *connectdb()
+
+	_, err := db.Exec(database)
+	if err != nil {
+		log.Fatalln(err)
+		return false
+	}
+	return true
+}
+
+func initdb() bool {
+	createdb(`CREATE TABLE IF NOT EXISTS "user" ("id" INTEGER PRIMARY KEY, "name" VARCHAR(255), "pass" VARCHAR(255))`)
+
+	return true
+}
+
+//User
+
+func addDBUser(user, pass string) bool {
+	db := connectdb()
+	addDb, err := db.Prepare(`INSERT INTO "user" ("name","pass") VALUES (?,?)`)
+	if err != nil {
+		panic(err)
+		return false
+	}
+
+	if _, err := addDb.Exec(user, hashgenerate(pass)); err != nil {
+		panic(err)
+		return false
+	}
+
+	return true
+}
+
+func deleteDBUser(name string) bool {
+	db := connectdb()
+	deleteDb := "DELETE FROM user WHERE name = ?"
+	_, err := db.Exec(deleteDb, name)
+	if err != nil {
+		log.Fatalln(err)
+		return false
+	}
+	return true
+}
+
+func TestPassDBUser(name, pass string) bool {
+	db := connectdb()
+	var hash string
+	if err := db.QueryRow("SELECT pass FROM user WHERE name = ?", name).Scan(&hash); err != nil {
+		log.Fatal(err)
+	}
+
+	return verifyhashdata(pass, hash)
+}
