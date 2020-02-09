37f15a8e812e3de90400bbf719170d6e226b517f
yoneyan <github@yoneyan.net>
2020-02-10 03:30:36 +0900

    add comment out

--- config.go
+++ config.go
@@ -33,29 +33,32 @@ var AConf AuthConfig
 var RConf RadiusConfig
 
 func config() {
-	c, _ := ini.Load("config.ini")
+	c, _ := ini.Load("config")
 	MConf = MainConfig{
 		RejectTime: c.Section("main").Key("reject_time").MustInt(),
 	}
-	WConf = WebConfig{
-		Port: c.Section("web").Key("port").MustInt(),
-	}
-	Nconf = NetConfig{
-		InboundInt:  c.Section("network_interface").Key("inbound").MustString("wlan0"),
-		OutboundInt: c.Section("network_interface").Key("outbound").MustString("eth0"),
-		LocalIP:     c.Section("network_ip").Key("local_ip").MustString("192.168.224.1"),
-		LocalSubnet: c.Section("network_ip").Key("local_subnet").MustInt(),
-		RangeStart:  c.Section("network_ip").Key("inbound").String(),
-		RangeEnd:    c.Section("network_ip").Key("inbound").String(),
-	}
-	AConf = AuthConfig{
-		AuthType: c.Section("authentication").Key("authtype").MustInt(),
-	}
-	RConf = RadiusConfig{
-		RadiusIP:   c.Section("authentication").Key("radius").String(),
-		RadiusUser: c.Section("authentication").Key("radius").String(),
-		RadiusPass: c.Section("authentication").Key("radius").String(),
-	}
+	/*
+		WConf = WebConfig{
+			Port: c.Section("web").Key("port").MustInt(),
+		}
+		Nconf = NetConfig{
+			InboundInt:  c.Section("network_interface").Key("inbound").MustString("wlan0"),
+			OutboundInt: c.Section("network_interface").Key("outbound").MustString("eth0"),
+			LocalIP:     c.Section("network_ip").Key("local_ip").MustString("192.168.224.1"),
+			LocalSubnet: c.Section("network_ip").Key("local_subnet").MustInt(),
+			RangeStart:  c.Section("network_ip").Key("inbound").String(),
+			RangeEnd:    c.Section("network_ip").Key("inbound").String(),
+		}
+		AConf = AuthConfig{
+			AuthType: c.Section("authentication").Key("authtype").MustInt(),
+		}
+		RConf = RadiusConfig{
+			RadiusIP:   c.Section("authentication").Key("radius").String(),
+			RadiusUser: c.Section("authentication").Key("radius").String(),
+			RadiusPass: c.Section("authentication").Key("radius").String(),
+		}
+
+	*/
 }
 
 func rejectTime() int {
